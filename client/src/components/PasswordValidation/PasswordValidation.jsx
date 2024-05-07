d1bbe4c82afa823a650205c06f5210f05b720c85
Mritunjay <you@example.com>
2024-05-08 00:47:55 +0530

    validation modal

--- client/src/components/PasswordValidation/PasswordValidation.jsx
+++ client/src/components/PasswordValidation/PasswordValidation.jsx
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
 import './PasswordValidation.css'
-
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const PasswordValidation = ({ value }) => {
+    let validationItem = [{ label: "6 Characters", key: "isSixCharacters" }, { label: "Small Letter", key: "isSmallLetter" }, { label: "Capital Letter", key: "isCapitalLetter" }, { label: "Special Character", key: "isSpecialCharacters" }, { label: "Number", key: "isNumber" },]
+    const [validateData, setValidateData] = useState({
+        isSixCharacters: false,
+        isSmallLetter: false,
+        isCapitalLetter: false,
+        isSpecialCharacters: false,
+        isNumber: false
+    })
+
     useEffect(() => {
-        console.log("value>>>>>>>>>>>>", value)
+        setValidateData((prevState => ({
+            ...prevState,
+            isSixCharacters: value.length >= 6,
+            isSmallLetter: /[a-z]/.test(value),
+            isCapitalLetter: /[A-Z]/.test(value),
+            isSpecialCharacters: /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g.test(value),
+            isNumber: /[0-9]/.test(value),
+        })))
+
     }, [value])
     return (
-        <div>
-            <p>Content</p>
-        </div>
+        <>
+            {validationItem && validationItem.length > 0 &&
+                validationItem.map((item, index) => {
+                    return (<div id={index}>
+                        <CheckCircleIcon className={`circle-icons ${validateData[item.key] ? "validText" : "inValidateText"}`} /> {item.label}
+                    </div>)
+                })
+            }
+        </>
     )
 }
 export default PasswordValidation;
