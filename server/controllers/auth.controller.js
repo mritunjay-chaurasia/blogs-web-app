b9abebbedcc565fdae50254fd8c614fe4d63ef2c
Mritunjay <you@example.com>
2024-05-11 10:27:37 +0530

    Sd

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+
+
+
+
+module.exports.findExistUser = async(req,res) => {
+
+}
+
+module.exports.register = async(req,res) =>{
+    console.log("Hellllllllllllllllllllllll  register",req.body)
+}
+
+module.exports.login = async(req,res)=>{
+    console.log("Hellllllllllllllllllllllll   login",req.body)
+}
+
+module.exports.requestResetPassword = async(req,res) => {
+
+}
+
+module.exports.updatePassword = async(req,res) => {
+
+}
+
+module.exports.deleteUser = async(req,res) => {
+
+}
